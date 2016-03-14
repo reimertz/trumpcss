864634507edd80e89c78b3dfd78fd636d7ae0fa0
Pierre Reimertz <pierre@pierrereimertz.com>
2016-03-14 22:17:51 +0100

    Update 2016-03-14T21:17:49.595Z

--- index.js
+++ index.js
@@ -3,3 +3,20 @@ var codes = document.querySelectorAll('code');
 [].forEach.call(codes, function(code) {
   hljs.highlightBlock(code);
 });
+
+
+var words = ['Trump', 'ignorance', 'racism', 'hypocrisy', 'stupidity'];
+var trumpSpan = document.querySelector('.trump-span');
+var index = 0;
+function startWords(){
+  setInterval(function(){
+
+    var word  = words[index];
+    trumpSpan.innerHTML = word;
+    trumpSpan.setAttribute('data-word', index);
+
+    index = ( (index + 1)  >= words.length) ? 0 : index + 1;
+  }, 1000);
+}
+
+setTimeout(startWords, 1000);
